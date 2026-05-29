191a7bd723238d4475f37d0f3bb82f5f6be2eb26
Atlantia22 <ktrivino2202@gmail.com>
2026-05-29 15:46:22 -0500

    Router dependencies

--- src/features/game/Home.tsx
+++ src/features/game/Home.tsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+import Button from "../../components/Button";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Tic Tac Toe</h1>
-      <Link to="/game">Start Game</Link>
+    <div className="container text-center mt-5">
+      <h1 className="mb-4">Welcome to Tic Tac Toe</h1>
+      <Link to="/game">
+        <Button variant="success">Start Game</Button>
+      </Link>
     </div>
   );
 }
