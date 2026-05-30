9d6a657d755e4558652c72f9b6cca5901f248027
Atlantia22 <ktrivino2202@gmail.com>
2026-05-29 23:40:52 -0500

    Save score to Firestore when a player wins

--- src/features/game/Game.tsx
+++ src/features/game/Game.tsx
@@ -1,11 +1,19 @@
 import Board from "./Board";
 import { useGame } from "../../context/GameContext";
 import Result from "./Result";
+import { useScores } from "../../hooks/useScores";
+import { useAuth } from "../../context/AuthContext";
 
 export default function Game() {
   const { turn, winner } = useGame();
+  const { saveScore } = useScores();
+  const { user } = useAuth();
 
+  // Cuando haya ganador, guardar en Firestore
   if (winner) {
+    if (user) {
+      saveScore(user.uid, winner);
+    }
     return <Result />;
   }
 
